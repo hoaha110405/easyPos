74f3bc161a3420ff5a7b8cac75f0e947227a364f
hoaha110405 <hoaha110404@gmail.com>
2025-08-08 10:59:20 +0700

    first setup

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+
+// import {connectDB} from './config/database'
+import app from './app.js'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+const startServer = async () => {
+    // await connectDB()
+    app.listen(PORT, ()=>{
+        console.log(`Server running on port ${PORT}`)
+    })
+}
+
+startServer();
