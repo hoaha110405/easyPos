2113dbb8a8cbd38fd1f157f222d2bef2e3f27a5a
minhpham2409 <providminh24092004.email@example.com>
2025-08-08 14:41:47 +0700

    updane db

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+dotenv.config();
 
-// import {connectDB} from './config/database'
-import app from './app.ts'
+import app from "./app.ts";
+import { connectDB } from "./config/database.ts";
 
-dotenv.config()
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
-
-const startServer = async () => {
-    // await connectDB()
-    app.listen(PORT, ()=>{
-        console.log(`Server running on port ${PORT}`)
-    })
-}
-
-startServer();
+(async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+  });
+})();
